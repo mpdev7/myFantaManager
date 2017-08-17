0e12892cdf3050ad3ab36c7e5680bf1bcc345855
Matteo Pedonese <matteo.pedonese@gmail.com>
2017-08-17 15:42:42 +0200

    aggiunto bottone per il back to top

---
+++ MyFantaManager/wwwroot/app/app.js
@@ -0,0 +1,17 @@
+﻿
+window.onscroll = function () { scrollFunction() };
+
+$('#scrollBtn').click(function (e) {
+    e.preventDefault();
+
+    $('body').animate({ scrollTop: 0 }, 300);
+});
+
+function scrollFunction() {
+    if (document.body.scrollTop > 250 || document.documentElement.scrollTop > 250) {
+        document.getElementById("scrollBtn").style.display = "block";
+    }
+    else {
+        document.getElementById("scrollBtn").style.display = "none";
+    }
+}
